bb56c9a2b51eca628f3f9b1aa5b78aa7bbf47f9b
Kehdinga Raymond <kehdingar@gmail.com>
2024-02-28 04:30:14 +0100

    test: add test for valid input

---
+++ tests/test_pagination_builder.py
@@ -0,0 +1,10 @@
+import pytest
+from ..pagination_builder import pagination_builder
+
+#test for valid inputs
+def test_valid_inputs():
+    assert pagination_builder(4, 5, 1, 0)  == "1 ... 4 5"
+    assert pagination_builder(4, 10, 2, 2)  == "1 2 3 4 5 6 ... 9 10"
+    assert pagination_builder(700, 9000, 3, 1)  == "1 2 3 ... 699 700 701 ... 8998 8999 9000"
+    assert pagination_builder(0, 0, 2, 2)  == ""
+
